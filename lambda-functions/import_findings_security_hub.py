3af397c22f06f959bb20dc8ea4a6a56128660f81
Moises Pablo Ortiz Tapia <moisestapia741@gmail.com>
2022-06-08 17:43:12 -0500

    Update files, add new version of Owasp DP 7.1.0 also fix the string in info in the step OWASP zap Proxy

--- lambda-functions/import_findings_security_hub.py
+++ lambda-functions/import_findings_security_hub.py
@@ -1,5 +1,5 @@
 """
- Imports finding in Security Hub and upload the reports to S3 
+ Imports finding in Security Hub and upload the reports to S3
  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  SPDX-License-Identifier: MIT-0
 """
@@ -7,9 +7,10 @@
 import os
 import json
 import logging
+from datetime import datetime, timezone
 import boto3
 import securityhub
-from datetime import datetime, timezone
+
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -19,7 +20,7 @@ FINDING_DESCRIPTION_TEMPLATE = "Summarized report of code scan with {0}"
 FINDING_TYPE_TEMPLATE = "{0} code scan"
 BEST_PRACTICES_PHP = "https://aws.amazon.com/developer/language/php/"
 BEST_PRACTICES_OWASP = "https://owasp.org/www-project-top-ten/"
-report_url = "https://aws.amazon.com"
+REPORT_URL = "https://aws.amazon.com"
 
 def process_message(event):
     """ Process Lambda Event """
@@ -38,9 +39,12 @@ def process_message(event):
         s3 = boto3.client('s3')
         s3bucket = "pipeline-artifact-bucket-" + account_id
         key = f"reports/{event['reportType']}/{build_id}-{created_at}.json"
-        s3.put_object(Bucket=s3bucket, Body=json.dumps(event), Key=key, ServerSideEncryption='aws:kms')
+        s3.put_object(Bucket=s3bucket,
+                    Body=json.dumps(event),
+                    Key=key,
+                    ServerSideEncryption='aws:kms')
         report_url = f"https://s3.console.aws.amazon.com/s3/object/{s3bucket}/{key}?region={region}"
-                
+
         ### OWASP SCA scanning report parsing
         if event['reportType'] == 'OWASP-Dependency-Check':
             severity = 50
@@ -51,14 +55,20 @@ def process_message(event):
                     confidence = event['report']['dependencies'][i]['packages'][0]['confidence']
                     url = event['report']['dependencies'][i]['packages'][0]['url']
                     finding_id = f"{i}-{report_type.lower()}-{build_id}"
-                    finding_description = f"Package: {event['report']['dependencies'][i]['packages'][0]['id']}, Confidence: {confidence}, URL: {url}"
+                    finding_description = f"Package: {event['report']['dependencies'][i]['packages'][0]['id']}, Confidence: {confidence}, URL: {url}" # pylint: disable=C0301
                     created_at = datetime.now(timezone.utc).isoformat()
                     ### find the vulnerability severity level
                     if confidence == "HIGHEST":
                         normalized_severity = 80
                     else:
                         normalized_severity = 50
-                    securityhub.import_finding_to_sh(i, account_id, region, created_at, source_repository, source_branch, source_commitid, build_id, report_url, finding_id, generator_id, normalized_severity, severity, finding_type, FINDING_TITLE, finding_description, BEST_PRACTICES_OWASP)
+                    securityhub.import_finding_to_sh(i, account_id, region, created_at,
+                                                source_repository, source_branch, source_commitid,
+                                                build_id, report_url, finding_id, generator_id,
+                                                normalized_severity, severity, finding_type,
+                                                FINDING_TITLE,
+                                                finding_description,
+                                                BEST_PRACTICES_OWASP)
 
         ### PHPStan SAST scanning report parsing
         if event['reportType'] == 'PHPStan':
@@ -68,26 +78,30 @@ def process_message(event):
             for i in range(report_count):
                 for filename in event['report']['files']:
                     finding_id = f"{i}-{report_type.lower()}-{build_id}"
-                    finding_description = f"Message: {event['report']['files'][filename]['messages'][0]['message']}, file: {filename}, line: {event['report']['files'][filename]['messages'][0]['line']}"
+                    finding_description = f"Message: {event['report']['files'][filename]['messages'][0]['message']}, file: {filename}, line: {event['report']['files'][filename]['messages'][0]['line']}" # pylint: disable=C0301
                     created_at = datetime.now(timezone.utc).isoformat()
-                    normalized_severity = 60                   
+                    normalized_severity = 60
                     ### find the vulnerability severity level
-                    is_ignorable = f"{event['report']['files'][filename]['messages'][0]['ignorable']}"
+                    is_ignorable = f"{event['report']['files'][filename]['messages'][0]['ignorable']}" # pylint: disable=C0301
                     if is_ignorable == "true":
                         normalized_severity = 30
                     else:
                         normalized_severity = 60
                     ### Calling Securityhub function to post the findings
-                    securityhub.import_finding_to_sh(i, account_id, region, created_at, source_repository, source_branch, source_commitid, build_id, report_url, finding_id, generator_id, normalized_severity, severity, finding_type, FINDING_TITLE, finding_description, BEST_PRACTICES_OWASP)               
-        
+                    securityhub.import_finding_to_sh(i, account_id, region, created_at,
+                                                source_repository, source_branch, source_commitid,
+                                                build_id, report_url, finding_id, generator_id,
+                                                normalized_severity, severity, finding_type,
+                                                FINDING_TITLE,
+                                                finding_description, BEST_PRACTICES_OWASP)
         ### SonarQube SAST scanning report parsing
-        elif event['reportType'] == 'SONAR-QUBE':           
+        elif event['reportType'] == 'SONAR-QUBE':
             severity = 50
-            FINDING_TITLE = "SonarQube StaticCode Analysis"         
+            FINDING_TITLE = "SonarQube StaticCode Analysis"
             report_count = event['report']['total']
             for i in range(report_count):
-                finding_id = f"{i}-{report_type.lower()}-{source_repository}-{source_branch}-{build_id}"
-                finding_description = f"{event['report']['issues'][i]['type']}-{event['report']['issues'][i]['message']}-{i}, component: {event['report']['issues'][i]['component']}"
+                finding_id = f"{i}-{report_type.lower()}-{source_repository}-{source_branch}-{build_id}" # pylint: disable=C0301
+                finding_description = f"{event['report']['issues'][i]['type']}-{event['report']['issues'][i]['message']}-{i}, component: {event['report']['issues'][i]['component']}" # pylint: disable=C0301
                 created_at = datetime.now(timezone.utc).isoformat()
                 report_severity = event['report']['issues'][i]['severity']
                 ### find the vulnerability severity level
@@ -100,10 +114,14 @@ def process_message(event):
                 else:
                     normalized_severity= 20
                 ### Calling Securityhub function to post the findings
-                securityhub.import_finding_to_sh(i, account_id, region, created_at, source_repository, source_branch, source_commitid, build_id, report_url, finding_id, generator_id, normalized_severity, severity, finding_type, FINDING_TITLE, finding_description, BEST_PRACTICES_OWASP)
-        
+                securityhub.import_finding_to_sh(i, account_id, region, created_at,
+                                            source_repository, source_branch, source_commitid,
+                                            build_id, report_url, finding_id, generator_id,
+                                            normalized_severity, severity, finding_type,
+                                            FINDING_TITLE,
+                                            finding_description, BEST_PRACTICES_OWASP)
         ### OWASP Zap SAST scanning report parsing
-        elif event['reportType'] == 'OWASP-Zap':  
+        elif event['reportType'] == 'OWASP-Zap':
             severity = 50
             FINDING_TITLE = "OWASP ZAP DynamicCode Analysis"
             alert_ct = event['report']['site'][0]['alerts']
@@ -116,22 +134,28 @@ def process_message(event):
                     normalized_severity = 70
                 elif riskletters == 'Med':
                     normalized_severity = 60
-                elif riskletters == 'Low' or riskletters == 'Inf':  
+                elif riskletters == 'Low' or riskletters == 'Info':
                     normalized_severity = 30
                 else:
-                    normalized_severity = 90                                       
+                    normalized_severity = 90
                 instances = len(event['report']['site'][0]['alerts'][alertno]['instances'])
-                finding_description = f"{alertno}-Vulerability:{event['report']['site'][0]['alerts'][alertno]['alert']}-Total occurances of this issue:{instances}"
+                finding_description = f"{alertno}-Vulerability:{event['report']['site'][0]['alerts'][alertno]['alert']}-Total occurances of this issue:{instances}" # pylint: disable=C0301
                 finding_id = f"{alertno}-{report_type.lower()}-{build_id}"
                 created_at = datetime.now(timezone.utc).isoformat()
                 ### Calling Securityhub function to post the findings
-                securityhub.import_finding_to_sh(alertno, account_id, region, created_at, source_repository, source_branch, source_commitid, build_id, report_url, finding_id, generator_id, normalized_severity, severity, finding_type, FINDING_TITLE, finding_description, BEST_PRACTICES_OWASP)
+                securityhub.import_finding_to_sh(alertno, account_id, region, created_at,
+                                            source_repository, source_branch, source_commitid,
+                                            build_id, report_url, finding_id, generator_id,
+                                            normalized_severity,
+                                            severity, finding_type, FINDING_TITLE,
+                                            finding_description,
+                                            BEST_PRACTICES_OWASP)
         else:
-            print("Invalid report type was provided")                
+            print("Invalid report type was provided")
     else:
         logger.error("Report type not supported:")
 
-def lambda_handler(event, context):
+def lambda_handler(event, context): # pylint: disable=W0613
     """ Lambda entrypoint """
     try:
         logger.info("Starting function")
@@ -139,4 +163,3 @@ def lambda_handler(event, context):
     except Exception as error:
         logger.error("Error {}".format(error))
         raise
-
